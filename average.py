49b9bc298225e7f2bf33eacbe6ef68ca729df8dc
Saud Albuainain <007728728@users.noreply.github.com>
2024-10-20 15:49:28 -0700

    Commit #2

---
+++ average.py
@@ -0,0 +1,8 @@
+print ('Please, enter five numbers:')
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+num5 = float(input("Enter the fifth number: "))
+average = (num1 + num2 + num3 + num4 + num5) / 5 # calculates the average of the five numbers entered
+print (f"The average of the five numbers is: {average}") # prints the result of the calculation
---
+++ temp_convert.py
@@ -0,0 +1,5 @@
+print ('Please enter a Celsius temperature.')
+C=float(input())  #convert to degrees Farenheit
+F=(9/5)*C+32
+print('The equivalent Fahrenheit temperature is:')
+print((F)) #display temperature in Farenheit
